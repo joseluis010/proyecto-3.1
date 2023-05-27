5c400c8edcd3fd1440a7eeb182940b855b7e5ed6
Jose Luis <joseluis.aguilar72@gmail.com>
2023-05-27 13:15:38 -0300

    admin

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Pais, Ciudad, Calle
 
-# Register your models here.
+
+
+class PaisAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+
+class CiudadAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'pais')
+    list_filter = ('pais',)
+
+
+class CalleAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ciudad')
+    list_filter = ('ciudad__pais', 'ciudad')
